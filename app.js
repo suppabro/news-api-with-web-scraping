ff02d53c196009378c13b807b17187cd31cb0817
suppabro <130851096+suppabro@users.noreply.github.com>
2023-11-08 11:07:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-const express= require('express');
-const app= express();
+const express = require('express');
 const puppeteer = require('puppeteer');
-const newsList=[];
-(async ()=>{
-    const browser = await puppeteer.launch({});
-    const page = await browser.newPage();
-    await page.goto('http://sinhala.adaderana.lk/sinhala-hot-news.php');
-    const titles = await page.$$('.story-text h2 a');
-    const descriptions= await page.$$('.story-text p'); 
-    const images= await page.$$('.story-text .thumb-image a img');
-    for (let i = 0; i < titles.length; i++) {
-        let title= await page.evaluate(el=> el.textContent, titles[i]);
-        let desc = await page.evaluate(el=>el.textContent,descriptions[i]);
-        let image = await page.evaluate(el=>el.getAttribute("src"), images[i]);
-        newsList.push({title:title,description:desc,imgLink:image});
+const app = express();
+const port = 3000;
+
+app.get('/', async (req, res) => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://sinhala.adaderana.lk/sinhala-hot-news.php');
+
+        const titles = await page.$$('.story-text h2 a');
+        const descriptions = await page.$$('.story-text p');
+        const images = await page.$$('.story-text .thumb-image a img');
+
+        const newsList = [];
+
+        for (let i = 0; i < titles.length; i++) {
+            let title = await page.evaluate(el => el.textContent, titles[i]);
+            let desc = await page.evaluate(el => el.textContent, descriptions[i]);
+            let image = await page.evaluate(el => el.getAttribute("src"), images[i]);
+            newsList.push({ title: title, description: desc, imgLink: image });
+        }
+
+        console.log('Scraped');
+        await browser.close();
+        res.send(newsList);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred during scraping.');
     }
-    console.log('scraped');
-    browser.close(); 
-})()
-
-app.get('/',(req,res)=>{
-    res.send(newsList);
-    
 });
 
-app.listen(3000, ()=>{
-    console.log('server started');
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
 });
-//to solve the timeout problem you can enter the following code after the page variable
-//    await page.setDefaultNavigationTimeout(0);
-// $=querySelector/ $$= querySelectorAll
-// const attr = await page.evaluate(el => el.getAttribute("href"), styleNumber);
-// let mathrukawa= await page.evaluate(el=> el.textContent, title);
